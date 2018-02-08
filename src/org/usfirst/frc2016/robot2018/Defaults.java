345a15bbd9e1b090e913a2ce21d1a85897fa1698
team2016 <team2016@774cc901-5b1c-4f7e-a1cd-09bd1ed89119>
2018-02-08 22:11:05 +0000

    Cleanup, more config

--- src/org/usfirst/frc2016/robot2018/Defaults.java
+++ src/org/usfirst/frc2016/robot2018/Defaults.java
@@ -16,18 +16,15 @@ package org.usfirst.frc2016.robot2018;
 // the cRio
 public class Defaults {
     
-    // Drive train voltage limit
+    // Drive train
     public static final double DRIVETRAIN_VOLTAGE_LIMIT_DEFAULT = .95;
+    public static final double DRIVETRAIN_RAMP_INCREMENT = .2;
+    public static final double DRIVETRAIN_P = .15;
+    public static final double DRIVETRAIN_I = 0;
+    public static final double DRIVETRAIN_D = 0;
+    public static final double DRIVETRAIN_F = .6;
 
-// Autonomous Time Defaults
-    public static final double AUT_SHOOTING_DELAY = .5;
-
-// Autonomous Other Defaults
-    public static final double AUT_ENCODER_DISTANCE_10FT = 100; //Distance in inches
-    public static final double AUT_RIGHT_Y = -.8;
-    public static final double AUT_LEFT_Y = .8;
-
-// Arm presets
+    // Arm presets
     public static final int ARMPOSITION0 = 2214;
     public static final int ARMPOSITION1 = 2300;
     public static final int ARMPOSITION2 = 2800;
@@ -38,10 +35,21 @@ public class Defaults {
     public static final double ARM_I = 0;
     public static final double ARM_D = 0;
 
-// Wheel Speeds
+    // Wheel Speeds
     public static final double WHEELSPEED_SHOOT = 1;
     public static final double WHEELSPEED_IN = .5;
     public static final double WHEELSPEED_OUT = .5;
     public static final double WHEELSPEED_ROTATE = .3;
+
+
+    // Autonomous Time Defaults
+     public static final double AUT_SHOOTING_DELAY = .5;
+
+     // Autonomous Other Defaults
+     public static final double AUT_ENCODER_DISTANCE_10FT = 100; //Distance in inches
+     public static final double AUT_RIGHT_Y = -.8;
+     public static final double AUT_LEFT_Y = .8;
+
+
 }
 
