6e1e747905e7949793a36db9aaf62a9cc6600823
Sterly Deracy <25871926+RoboNinjaSterlz@users.noreply.github.com>
2018-02-14 02:11:23 +0000

    changed logic to determine when we are done with magic move, several config file changes

--- src/org/usfirst/frc2016/robot2018/commands/MagicDrive.java
+++ src/org/usfirst/frc2016/robot2018/commands/MagicDrive.java
@@ -57,7 +57,7 @@ public class MagicDrive extends Command {
     // Make this return true when this Command no longer needs to run execute()
     @Override
     protected boolean isFinished() {
-        return Robot.driveTrainSRX.moveComplete();
+       return Robot.driveTrainSRX.moveComplete();
     }
 
     // Called once after isFinished returns true
