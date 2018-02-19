b57e93a13cc393ea731876712d882174007e1dc5
team2016 <team2016@774cc901-5b1c-4f7e-a1cd-09bd1ed89119>
2018-02-19 17:22:31 +0000

    put gyro_p back to .09

--- src/org/usfirst/frc2016/robot2018/subsystems/Gyro.java
+++ src/org/usfirst/frc2016/robot2018/subsystems/Gyro.java
@@ -26,7 +26,7 @@ import edu.wpi.first.wpilibj.SPI;
  */
 public class Gyro extends Subsystem {
 	
-	public final double gyroP = 0.01;
+	public final double gyroP = 0.09;
 	public final double TURN_MAX = 0.6;
 	private final String GYRO_ANGLE = "Gyro Angle";
 	private final String GYRO_ROLL = "Gyro Roll";
