108f4ea74801c33272f0213644bea9f5cc8e2056
Sterly Deracy <25871926+RoboNinjaSterlz@users.noreply.github.com>
2018-04-24 00:05:11 +0000

    AutoPilot Shop Tested

--- src/org/usfirst/frc2016/robot2018/commands/AutoPilotCommand.java
+++ src/org/usfirst/frc2016/robot2018/commands/AutoPilotCommand.java
@@ -1,189 +1,69 @@
 package org.usfirst.frc2016.robot2018.commands;
 
 import org.usfirst.frc2016.robot2018.Robot;
-import org.usfirst.frc2016.robot2018.commands.AutoPilot.AutoPilotMethod;
+import org.usfirst.frc2016.robot2018.AutoPilot.AutoPilotMethod;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import java.util.Objects;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class AutoPilotCommand extends Command {
-
-	private String moveFilePath = "/c/";
-	/** Default: /c folder. */
-	// --private String moveFilePath = "src\\testDrive\\";
-
-	AutoPilot autoPilot;
-	String lastStatus;
-	Timer driveUpdateTimer;
-	int nDriveUpdateMs = 10;
-
-	public enum ArmState {
-		OPEN, CLOSE
+// class to start AutoPilot sequences
+public class AutoPilotCommand extends CommandGroup {
+	String sequenceKey;
+	Boolean initFromFile = false;
+	
+	public AutoPilotCommand(String sequenceKey) {
+		this.sequenceKey = sequenceKey;
+		
+        requires(Robot.driveTrainSRX);
+        requires(Robot.cubePickup);
+        requires(Robot.arm);
 	}
-
-	public enum IntakeState {
-		IN, OUT, STOP
+	
+	public AutoPilotCommand(String sequenceKey, Boolean loadFile) {
+		this.sequenceKey = sequenceKey;
+		initFromFile = loadFile;
+		
+        requires(Robot.driveTrainSRX);
+        requires(Robot.cubePickup);
+        requires(Robot.arm);
 	}
-
-	public enum LiftPosition {
-		FLOOR, LOW, MEDIUM, SCALE, HIGH
-	}
-
-	public AutoPilotCommand() {
-		autoPilot = new AutoPilot(this);
-		driveUpdateTimer = new Timer("DriveUpdateThread", true);
-		// autoPilot.listCommands();
-
-		autoPilot.loadFile(moveFilePath + "AutoPilot.txt");
-		SmartDashboard.putString("AutoPilotStatus", autoPilot.getStatus());
-
-		if (!autoPilot.isTracing()) {
-			lastStatus = autoPilot.getStatus();
-			System.out.println(lastStatus);
-		}
-	}
-
+	
 	// Called just before this Command runs the first time
 	@Override
 	protected void initialize() {
-		Robot.driveTrainSRX.resetEncoders();
-		autoPilot.setTracing(false);
-		autoPilot.startSequence("2Cube-L", getDriveDistances());
-		startTimerTask();
+		if (initFromFile) {
+			Robot.autoPilotRobot.loadFile();
+		}
+		Robot.autoPilotRobot.initialize(sequenceKey);
 	}
 
 	// Called repeatedly when this Command is scheduled to run
 	@Override
 	public void execute() {
-
-		Robot.driveTrainSRX.pingDifferentialDrive();
-		SmartDashboard.putString("AutoPilotStatus", autoPilot.getStatus());
-
-		if (!autoPilot.isTracing()) {
-			String status = autoPilot.getStatus();
-			if (!Objects.equals(lastStatus, status)) {
-				lastStatus = status;
-				System.out.println(lastStatus);
-			}
-		}
-
+		Robot.autoPilotRobot.execute();
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
-	// --@Override
+	@Override
 	protected boolean isFinished() {
-		return autoPilot.isFinished();
+		return Robot.autoPilotRobot.isFinished();
 	}
 
 	// Called once after isFinished returns true
-	// --@Override
+	@Override
 	protected void end() {
-		autoPilot.setTracing(true);
-		autoPilot.end();
-		shutdown();
+		Robot.autoPilotRobot.end();
 	}
 
 	// Called when another command which requires one or more of the same
 	// subsystems is scheduled to run
-	// --@Override
+	@Override
 	protected void interrupted() {
-		autoPilot.interrupted();
-		shutdown();
-	}
-
-	// called via reflection to get the current distance from the drive train
-	private Double[] getDriveDistances() {
-		Double[] distances = new Double[AutoPilot.MOTOR_MAX];
-		distances[AutoPilot.MOTOR_LEFT] = 0.0;
-		distances[AutoPilot.MOTOR_RIGHT] = 0.0;
-		
-		Robot.driveTrainSRX.resetEncoders();
-		distances[AutoPilot.MOTOR_LEFT] = Robot.driveTrainSRX.getLeftDistance();
-		distances[AutoPilot.MOTOR_RIGHT] = Robot.driveTrainSRX.getRightDistance();
-		return distances;
-	}
-
-	private void shutdown() {
-		killTimerTask();
-		Robot.cubePickup.autoEnd();
-		Robot.driveTrainSRX.driveStop();
-	}
-
-	private void startTimerTask() {
-		killTimerTask();
-		driveUpdateTimer = new Timer();
-		driveUpdateTimer.scheduleAtFixedRate(new DriveTimerTask(),
-				nDriveUpdateMs, nDriveUpdateMs);
-	}
-
-	private void killTimerTask() {
-		if (null != driveUpdateTimer) {
-			driveUpdateTimer.cancel();
-			driveUpdateTimer.purge();
-			driveUpdateTimer = null;
-		}
-	}
-
-	class DriveTimerTask extends TimerTask {
-		@Override
-		public void run() {
-			if (autoPilot.isFinished()) {
-				killTimerTask();
-			} else {
-				double[] driveDistances = autoPilot.execute();
-				if (null != driveDistances) {
-					
-					Robot.driveTrainSRX.goToDistance(
-							driveDistances[AutoPilot.MOTOR_LEFT],
-							driveDistances[AutoPilot.MOTOR_RIGHT]);
-				}
-			}
-		}
-	}
-
-	@AutoPilotMethod(argHint = "( Open|Close ) : moves the intake arms")
-	public void intakeArms(ArmState newArmState) {
-
-		switch (newArmState) {
-
-		case OPEN:
-			Robot.cubePickup.openArms();
-			break;
-
-		default:
-		case CLOSE:
-			Robot.cubePickup.closeArms();
-			break;
-
-		}
-	}
-
-	@AutoPilotMethod(argHint = "( In|Out|Stop ) : sets the intake wheels")
-	public void intakeWheels(IntakeState newIntakeState) {
-
-		switch (newIntakeState) {
-
-		case IN:
-			Robot.cubePickup.acquireCube();
-			break;
-
-		case OUT:
-			Robot.cubePickup.autoEjectCube();
-			break;
-
-		default:
-		case STOP:
-			Robot.cubePickup.autoEnd();
-			break;
-		}
-	}
-
-	@AutoPilotMethod(argHint = "( Floor|Low|Medium|Scale|High ) : target for the lift arm")
-	public void liftTarget(LiftPosition newTarget) {
-		Robot.arm.goToPreset(newTarget.ordinal());
+		Robot.autoPilotRobot.interrupted();
 	}
 }
